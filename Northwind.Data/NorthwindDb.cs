0bf7ad386a3860b307f214a9ecb87444ace236a5
Immo Landwerth <immol@microsoft.com>
2019-03-07 07:20:08 -0600

    Extract .NET Standard 2.0 lib

--- Northwind.Data/NorthwindDb.cs
+++ Northwind.Data/NorthwindDb.cs
@@ -9,29 +9,29 @@ namespace Northwind
     {
         public static string GetData()
         {
-            string result = "";
-            DataSet dataSet = new DataSet();
+            var result = "";
+            var dataSet = new DataSet();
             dataSet.ReadXml(@"C:\demos\create-netstandard\northwind.xml");
 
-            DataContext dataContext = new DataContext();
+            var dataContext = new DataContext();
             dataContext.AddTablesAndRelations(dataSet);
 
-            string sql = @"
+            var sql = @"
                 SELECT  FirstName + ' ' + LastName Name,
                         Birthdate.AddYears(65) RetirementDate
                 FROM    Employees
                 WHERE   Birthdate.AddYears(65) < GETDATE()
             ";
 
-            Query query = new Query(sql, dataContext);
-            DataTable resultsTable = query.ExecuteDataTable();
+            var query = new Query(sql, dataContext);
+            var resultsTable = query.ExecuteDataTable();
 
             foreach (DataRow row in resultsTable.Rows)
             {
-                string name = Convert.ToString(row["Name"]);
-                DateTime retirementDate = Convert.ToDateTime(row["RetirementDate"]); ;
+                var name = Convert.ToString(row["Name"]);
+                var retirementDate = Convert.ToDateTime(row["RetirementDate"]); ;
 
-                result += name + ": " + retirementDate + "\r\n";
+                result += $"{name}: {retirementDate}\r\n";
             }
 
             return result;
